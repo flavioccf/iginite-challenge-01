5450d1638eda18fd200e87ebb21b05b2647a7b78
flaviocc <flaviocc@hotmail.com>
2021-08-31 19:40:35 -0700

    challenge 1 solution

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-
 import '../styles/tasklist.scss'
 
 import { FiTrash, FiCheckSquare } from 'react-icons/fi'
@@ -15,15 +14,23 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (newTaskTitle.length > 0)
+    setTasks([...tasks, {
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false
+    }])
+    setNewTaskTitle('')
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const toggledArray = tasks.map(task => task.id !== id ? task : {...task, isComplete: !task.isComplete})
+    setTasks(toggledArray)
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    const removedTaskArray = tasks.filter(task => task.id !== id)
+    setTasks(removedTaskArray)
   }
 
   return (
